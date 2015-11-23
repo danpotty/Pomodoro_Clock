240bcb460f98030b4dffdff5a9af682551656043
danpotty <danpotmail@yahoo.com>
2015-11-22 22:09:06 -0800

    reset timer working

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,7 @@
 var timerSecs = 5; timerSecsSave = 5; breakSecs = 3; breakSecsSave = 3;
 var timer, timer2;
 var stopDecA = 0; stopDecB = 0;
+var gtg = true;
 
 function setUp(){
   document.getElementById('status').innerHTML = "<strong>"+0+"</strong>";
@@ -48,29 +49,38 @@ function subOneBreak(){
 }
 
 function countDown() {
-	document.getElementById('status').innerHTML = "<strong>"+timerSecs+"</strong>";
-	if(timerSecs < 0) {
-		clearTimeout(timer);
-    breakSecs = breakSecsSave;
-    runBreak();
-    return
-	}
-	timerSecs--;
-	var timer = setTimeout('countDown()',1000);
+  if (gtg) {
+  	document.getElementById('status').innerHTML = "<strong>"+timerSecs+"</strong>";
+  	if(timerSecs < 0) {
+  		clearTimeout(timer);
+      breakSecs = breakSecsSave;
+      runBreak();
+      return
+  	}
+  	timerSecs--;
+  	var timer = setTimeout('countDown()',1000);
+  } else { return }
 }
 
 function runBreak(){
-  document.getElementById('status').innerHTML = "<strong>"+breakSecs+"</strong>";
-  if(breakSecs < 0) {
-    clearTimeout(timer2);
-    timerSecs = timerSecsSave;
-    countDown();
-    return
-  }
-  breakSecs--;
-  var timer2 = setTimeout('runBreak()',1000);
+  if (gtg) {
+    document.getElementById('status').innerHTML = "<strong>"+breakSecs+"</strong>";
+    if(breakSecs < 0) {
+      clearTimeout(timer2);
+      timerSecs = timerSecsSave;
+      countDown();
+      return
+    }
+    breakSecs--;
+    var timer2 = setTimeout('runBreak()',1000);
+  } else { return }
 }
 
-function stopTimer(){
+function setTrue(){
+  gtg = true;
+}
 
+function resetTimer(){
+  gtg = false; timerSecs = 5; timerSecsSave = 5; breakSecs = 3; breakSecsSave = 3;
+  setUp();
 }
